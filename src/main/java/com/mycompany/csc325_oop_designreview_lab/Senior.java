5467abf573ac83561ecb1a57a433b154a8cd2694
jonathan-crain <124228802+jonathan-crain@users.noreply.github.com>
2023-03-09 19:44:45 -0500

    Update Senior.java
    Now extends Student. Added constructor and toString.

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/Senior.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Senior.java
@@ -4,6 +4,21 @@ package com.mycompany.csc325_oop_designreview_lab;
  *
  * @author Jonathan Crain
  */
-public class Senior {
-    
+public class Senior extends Student {
+
+    int credits;
+
+    public Senior(String name, short age, int credits) throws InsufficientCreditsException {
+        super(name, age);
+        if (credits < 85) {
+            throw new InsufficientCreditsException("Insufficient credits. Senior must have 85 or more credits.");
+        } else {
+            this.credits = credits;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Senior{" + "credits=" + credits + '}';
+    }
 }
