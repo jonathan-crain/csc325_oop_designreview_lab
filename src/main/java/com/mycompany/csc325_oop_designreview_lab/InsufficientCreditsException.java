10163efb2722b5bbe990d0e765e32799be8d55bf
jonathan-crain <124228802+jonathan-crain@users.noreply.github.com>
2023-03-09 19:38:34 -0500

    Create InsufficientCreditsException.java
    For use when a Senior (or other subtype of student) is created without sufficient credits.

---
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/InsufficientCreditsException.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.csc325_oop_designreview_lab;
+
+/**
+ *Exception thrown if a subtype of Student is created with insufficient credits for that subtype.
+ * @author niarc
+ */
+public class InsufficientCreditsException extends Exception{
+    public InsufficientCreditsException(String errorMessage, Throwable err){
+        super(errorMessage, err);
+    }
+}
