f4f2bad2ddebf8e60a2b3cdc20f97c49c2e3c6bf
jonathan-crain <124228802+jonathan-crain@users.noreply.github.com>
2023-03-09 19:23:13 -0500

    Update Freshman.java
    Added toString to Freshman (ToDo #8)

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/Freshman.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Freshman.java
@@ -4,6 +4,16 @@ package com.mycompany.csc325_oop_designreview_lab;
  *
  * @author Jonathan Crain
  */
-public class Freshman {
+public class Freshman extends Student{
+    int credits;
     
+    public Freshman (String name, short age, int credits){
+        super(name, age);
+        this.credits = credits;
+    }
+
+    @Override
+    public String toString() {
+        return "Freshman{" + "credits=" + credits + '}';
+    }
 }
